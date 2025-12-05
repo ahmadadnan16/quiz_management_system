6f7c23d857960299846210e203e92920b895adb1
adnan ahmad <ahmadadnan1602@gmail.com>
2025-12-05 18:01:32 +0530

    Fixed the quiz submission issue

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -7,10 +7,18 @@ class SubmissionsController < ApplicationController
     
     QuizGradingService.new(@submission, answers_params).grade!
     
-    redirect_to quiz_results_path(@quiz, @submission)
+    redirect_to quiz_results_path(@quiz, @submission), notice: 'Quiz submitted successfully!'
   rescue ActiveRecord::RecordInvalid => e
     @questions = @quiz.questions.ordered.includes(:options)
-    flash.now[:alert] = 'There was an error submitting your quiz. Please try again.'
+    Rails.logger.error "Submission error: #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
+    flash.now[:alert] = "There was an error submitting your quiz: #{e.message}"
+    render 'quizzes/show', status: :unprocessable_entity
+  rescue StandardError => e
+    @questions = @quiz.questions.ordered.includes(:options)
+    Rails.logger.error "Submission error: #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
+    flash.now[:alert] = "There was an error submitting your quiz. Please try again."
     render 'quizzes/show', status: :unprocessable_entity
   end
   
